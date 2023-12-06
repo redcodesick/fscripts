0cb9fee3929e0a9fda525c575f26e237c3c996c3
(sic) <sic@redcodenetwork.ro>
2023-12-06 10:39:39 +0200

    Update teams.wake.sh

--- teams.wake.sh
+++ teams.wake.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+
+stop_script() {
+    echo "Stopping script..."
+    pkill -f 'caffeinate -d'
+    exit 0
+}
+
+trap stop_script SIGINT
+
 caffeinate -d &
+
 while true;
 do
 #    osascript -e 'tell application "Microsoft Teams" to activate'
-#    osascript -e 'tell application "Microsoft Teams classic" to activate'
+    osascript -e 'tell application "Microsoft Teams classic" to activate'
     osascript -e 'tell application "System Events" to keystroke "2" using {command down}'
 
-   echo "Teams Status Refreshed"
+    echo "Teams Status Refreshed"
     sleep 300
 done
